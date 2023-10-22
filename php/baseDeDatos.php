e46c0925378ce981e206b121ffd136a4b38a5e0c
Alexander Lopez <61158846+UO266499@users.noreply.github.com>
2023-10-22 17:08:39 +0200

    Update baseDeDatos.php

--- php/baseDeDatos.php
+++ php/baseDeDatos.php
@@ -89,12 +89,12 @@ $id = 0;$name = 'Museo historia de Armenia';$location = 'Erevan';$price = 7;$typ
 }
 
 public function crearCuenta(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
-    $queryData = $connection->prepare("INSERT INTO users (user_id,username,password,email) VALUES (?,?,?,?)");
+     $this->createConnection();
+    $queryData = $this->connection->prepare("INSERT INTO users (user_id,username,password,email) VALUES (?,?,?,?)");
     if(empty($_POST["username"]) || empty($_POST["email"]) || empty($_POST["password"])){
         echo "<p> Alguno de los campos no ha sido rellenado </p>";
     }else{
-        $query = $connection->query("SELECT COUNT(*) from users WHERE username = '" . $_POST["username"] ."'");
+        $query = $this->connection->query("SELECT COUNT(*) from users WHERE username = '" . $_POST["username"] ."'");
         if($query->fetch_row()[0] != 0){
             echo "<p>Ya existe una cuenta con ese usuario</p>";
         }else{
@@ -104,26 +104,26 @@ public function crearCuenta(){
         echo "<p>Cuenta anadida correctamente</p>";
         }
     }
-    $connection->close();
+    $this->connection->close();
 }
 
 public function inicioSesion(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
-    $query = $connection->query("SELECT COUNT(*) from users WHERE username = '" . $_POST["username2"] ."' AND password = '" . $_POST["password2"] ."'");
+ $this->createConnection();
+    $query = $this->connection->query("SELECT COUNT(*) from users WHERE username = '" . $_POST["username2"] ."' AND password = '" . $_POST["password2"] ."'");
     if($query->fetch_row()[0] == 0){
         echo "<p>No existe esta cuenta</p>";
     }else{
-        $query = $connection->query("SELECT user_id from users WHERE username = '" . $_POST["username2"] ."' AND password = '" . $_POST["password2"] ."'");
+        $query = $this->connection->query("SELECT user_id from users WHERE username = '" . $_POST["username2"] ."' AND password = '" . $_POST["password2"] ."'");
         $_SESSION['user_id'] = $query->fetch_row()[0];
         echo "<p>Bienvenido " . $_POST["username2"] . ", haga click en el enlace inferior para acceder a los recursos turisticos.</p>";
         echo '<a href="reservar.php" title="Iniciar busqueda de reservas">Buscar recursos turisticos a reservar</a>';
     }
     
-    $connection->close();
+    $this->connection->close();
 }
 
 public function mostrarRecursos(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
+  $this->createConnection();
     if(empty($_POST["fecha"]) || empty($_POST["time"]) || empty($_POST["fechaF"]) || empty($_POST["timeF"])){
         echo "<p> Se debe seleccionar una fecha y hora antes de buscar </p>";
     }else{
@@ -135,14 +135,14 @@ public function mostrarRecursos(){
         $_SESSION['fechaF'] = $fechaF;
         $timeF = $_POST["timeF"];
         $_SESSION['timeF'] = $timeF;
-        $query = $connection->query("SELECT * from tourist_resources");
+        $query = $this->connection->query("SELECT * from tourist_resources");
         
         echo "<form action='#' method='post' id=reserva>";
 
         while ($row = $query->fetch_row()){
-            $capacidadActual = $connection->query("SELECT COUNT(*) FROM reservations where trip_date ='".$fecha."'AND trip_time ='".$time."' AND resource_id ='".$row[0]."'");
+            $capacidadActual = $this->connection->query("SELECT COUNT(*) FROM reservations where trip_date ='".$fecha."'AND trip_time ='".$time."' AND resource_id ='".$row[0]."'");
             $capacidad = $capacidadActual->fetch_row()[0];
-            $review = $connection->query("SELECT AVG(rating) from reviews WHERE resource_id = " . $row[0]);
+            $review = $this->connection->query("SELECT AVG(rating) from reviews WHERE resource_id = " . $row[0]);
             echo "<h3>" . $row[1]. "</h3>";
             echo "<p> Ciudad: " . $row[2]. "</p>";
             echo "<p>Valoracion media: ". round($review->fetch_row()[0],4). "</p>";
@@ -171,16 +171,16 @@ public function mostrarRecursos(){
 }
 
 public function verReservas(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
+    $this->createConnection();
     echo "<h2> Mis reservas </h2>";
     echo "<p> En la siguiente sección se muestran las reservas realizadas hasta ahora. </p>";
-    $query = $connection->query("SELECT * from reservations WHERE user_id= " . $_SESSION['user_id'] . " ORDER BY  trip_date, trip_time ASC ");
+    $query = $this->connection->query("SELECT * from reservations WHERE user_id= " . $_SESSION['user_id'] . " ORDER BY  trip_date, trip_time ASC ");
     while ($row = $query->fetch_row()){
-        $resource = $connection->query("SELECT * from tourist_resources WHERE resource_id= " . $row[2]);
+        $resource = $this->connection->query("SELECT * from tourist_resources WHERE resource_id= " . $row[2]);
         while ($rowResource = $resource->fetch_row()){
-            $isReviewed = $connection->query("SELECT * from reviews WHERE resource_id = " . $rowResource[0] . " AND user_id =" .$_SESSION['user_id']);
+            $isReviewed = $this->connection->query("SELECT * from reviews WHERE resource_id = " . $rowResource[0] . " AND user_id =" .$_SESSION['user_id']);
             $rowIsReviewed = $isReviewed->fetch_row();
-            $paymentMethod = $connection->query("SELECT payment_method from payments WHERE reservation_id = " . $row[0]);
+            $paymentMethod = $this->connection->query("SELECT payment_method from payments WHERE reservation_id = " . $row[0]);
             $initDate =DateTime::createFromFormat('Y-m-d',$row[3]);
             $finDate = DateTime::createFromFormat('Y-m-d',$row[5]);
             if($rowIsReviewed == null){
@@ -212,7 +212,7 @@ public function verReservas(){
 }
 
 public function reservar(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
+ $this->createConnection();
     $metodo = $_POST['metodoDePago'];
     $valido = true;
     $fecha = $_SESSION['fecha'];
@@ -223,9 +223,9 @@ public function reservar(){
             $dateFinal = DateTime::createFromFormat('Y-m-d', $_SESSION['fechaF']);
             while(!($dateInicial == $dateFinal)){
                 $currentDate =  date_format($dateInicial,'Y-m-d');
-                $queryAmount = $connection->query("SELECT COUNT(*) FROM  reservations WHERE trip_date='$currentDate' AND trip_time='$hora'"); 
+                $queryAmount = $this->connection->query("SELECT COUNT(*) FROM  reservations WHERE trip_date='$currentDate' AND trip_time='$hora'"); 
 
-                $query = $connection->query("SELECT availability FROM tourist_resources where resource_id = ". $key );
+                $query = $this->connection->query("SELECT availability FROM tourist_resources where resource_id = ". $key );
 
                 $availability = $query->fetch_row()[0];
                 $numberOfReservations = $queryAmount->fetch_row()[0];
@@ -240,8 +240,8 @@ public function reservar(){
     if ($valido){
     foreach($_POST as $key => $value){
         if($key != 'reservar' && $key != 'metodoDePago'){
-            $queryData = $connection->prepare("INSERT INTO reservations (reservation_id,user_id,resource_id,trip_date,trip_time, trip_date_end, trip_time_end,total_cost) VALUES (?,?,?,?,?,?,?,?)"); 
-            $query = $connection->query("SELECT price FROM tourist_resources where resource_id = ". $key );
+            $queryData = $this->connection->prepare("INSERT INTO reservations (reservation_id,user_id,resource_id,trip_date,trip_time, trip_date_end, trip_time_end,total_cost) VALUES (?,?,?,?,?,?,?,?)"); 
+            $query = $this->connection->query("SELECT price FROM tourist_resources where resource_id = ". $key );
 
             $id= 0;
             $user_id = $_SESSION['user_id'];
@@ -266,21 +266,21 @@ public function reservar(){
             
             $queryData ->execute();
 
-            $reservation_id = $connection->query("SELECT reservation_id FROM reservations where resource_id = ". $resource_id ." AND user_id=" . $_SESSION['user_id']." AND trip_date='" . $date ."' AND trip_time= '" . $time ."'"   );
+            $reservation_id = $this->connection->query("SELECT reservation_id FROM reservations where resource_id = ". $resource_id ." AND user_id=" . $_SESSION['user_id']." AND trip_date='" . $date ."' AND trip_time= '" . $time ."'"   );
 
-            $connection->query("INSERT INTO payments (payment_id,reservation_id,payment_method,amount) VALUES (". 0 .",".$reservation_id->fetch_row()[0].",'".$metodo."',".$price.")");
+            $this->connection->query("INSERT INTO payments (payment_id,reservation_id,payment_method,amount) VALUES (". 0 .",".$reservation_id->fetch_row()[0].",'".$metodo."',".$price.")");
         }
     }
-    $query = $connection->query("SELECT SUM(total_cost) FROM reservations where user_id = ". $_SESSION['user_id'] );
+    $query = $this->connection->query("SELECT SUM(total_cost) FROM reservations where user_id = ". $_SESSION['user_id'] );
     echo "Coste total de reservas: " . $query->fetch_row()[0];
 }
-}
+}F
 
 public function hacerResena(){
-    $connection = new mysqli($this->server,$this->username,$this->password,$this->db);
+   $this->createConnection();
     foreach($_POST as $key => $value){
         if($key != 'resena' && $key != 'comentarios'){
-            $queryData = $connection->prepare("INSERT INTO reviews (review_id,user_id,resource_id,rating,comments) VALUES (?,?,?,?,?)"); 
+            $queryData = $this->connection->prepare("INSERT INTO reviews (review_id,user_id,resource_id,rating,comments) VALUES (?,?,?,?,?)"); 
             $review_id = 0;
             $user_id = $_SESSION['user_id'];
             $resource_id = $key;
